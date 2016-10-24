d54dfb68d2ae539be0a28dce1b8b394394fc8538
Sara <saraahmad1114@yahoo.com>
2016-10-23 23:03:01 -0400

    changes

--- NASAAPI/NASAAPI/AsteriodTableViewController.swift
+++ NASAAPI/NASAAPI/AsteriodTableViewController.swift
@@ -15,43 +15,43 @@ class AsteriodTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        let qualityOfServiceClass = QOS_CLASS_BACKGROUND
-//        let backgroundQueue = dispatch_get_global_queue(qualityOfServiceClass, 0)
-//        dispatch_async(backgroundQueue, {
+        let qualityOfServiceClass = QOS_CLASS_UTILITY
+        let backgroundQueue = dispatch_get_global_queue(qualityOfServiceClass, 0)
+        dispatch_async(backgroundQueue, {
         
-//        self.store.getAsteriodInformationWithCompletion("2015-09-07", endData: "2015-09-08") { (nasaArray) in
-//                print("THIS IS PRINTING")
-//                print("***************************")
-//                print(nasaArray)
-//                print("***************************")
-//                self.tableView.reloadData()
-//        }
-//            })
-//        NSOperationQueue.mainQueue().addOperationWithBlock {
-//            self.tableView.reloadData()
-//        }
-        
-    let priority = DISPATCH_QUEUE_PRIORITY_DEFAULT
-    dispatch_async(dispatch_get_global_queue(priority, 0)) {
-    self.store.getAsteriodInformationWithCompletion("2015-09-07", endData: "2015-09-08") { (nasaArray) in
-        print("THIS IS PRINTING")
-        print("***************************")
-        print(nasaArray)
-        print("***************************")
-        self.tableView.reloadData()
+        self.store.getAsteriodInformationWithCompletion("2015-09-07", endData: "2015-09-08") { (nasaArray) in
+                print("THIS IS PRINTING")
+                print("***************************")
+                print(nasaArray)
+                print("***************************")
+                self.tableView.reloadData()
         }
-    }
-        dispatch_async(dispatch_get_main_queue()) {
+            })
+        NSOperationQueue.mainQueue().addOperationWithBlock {
             self.tableView.reloadData()
         }
+        
+//    let priority = DISPATCH_QUEUE_PRIORITY_DEFAULT
+//    dispatch_async(dispatch_get_global_queue(priority, 0)) {
+//    self.store.getAsteriodInformationWithCompletion("2015-09-07", endData: "2015-09-08") { (nasaArray) in
+//        print("THIS IS PRINTING")
+//        print("***************************")
+//        print(nasaArray)
+//        print("***************************")
+//        self.tableView.reloadData()
+//        }
+//    }
+//        dispatch_async(dispatch_get_main_queue()) {
+//            self.tableView.reloadData()
+//        }
     }
 
 
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(true)
-        NSOperationQueue.mainQueue().addOperationWithBlock {
-           self.tableView.reloadData()
-           }
+//        NSOperationQueue.mainQueue().addOperationWithBlock {
+//           self.tableView.reloadData()
+//           }
     }
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
